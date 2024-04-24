9af168760e257340b34077149fe030e52139cc84
gabriel <gabovchoque@gmail.com>
2024-04-24 17:01:33 -0400

    Relacion modelos Eloquent ORM

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function persona(){
+        return $this->hasOne(Persona::class);
+    }
 }
